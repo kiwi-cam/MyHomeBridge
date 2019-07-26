6667fb083e883ab28c0a2bc371a2d6119cd9dc9a
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2019-07-26 12:44:36 +1200

    Update disablecamera.sh
    Added error handling and logging

--- var/homebridge/disablecamera.sh
+++ var/homebridge/disablecamera.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
 
-ssh -i /var/homebridge/id_rsa -o StrictHostKeyChecking=no admin@192.168.21.51 '/data/etc/enablesingle.sh &> /dev/null'
+ssh -i /var/homebridge/id_rsa -o StrictHostKeyChecking=no admin@motioneye '/data/etc/enablesingle.sh &> /dev/null'
 rm -f /var/tmp/indoorcamera.state
+echo 'Indoor Camera coming offline' | systemd-cat -p info
+
+#Check for success
+counter=0
+while nc -z motioneye 8082; do
+        echo i"Disabling Indoor Camera failed - retrying ($counter)" | systemd-cat -p warning
+        counter=`expr $counter + 1`
+        sleep 5
+        ssh -i /var/homebridge/id_rsa -o StrictHostKeyChecking=no admin@motioneye '/data/etc/enablesingle.sh &> /dev/null'
+
+        if [ $counter -eq 5 ]; then
+           break
+        fi
+done
+
+#Check if attempts failed and log results
+if [ $counter -eq 5 ]; then
+        echo "Indoor Camera failed after $counter attempts - ABORTED" | systemd-cat -p err
+else
+        #Update status file
+        rm -f /var/tmp/indoorcamera.state
+        echo 'Indoor Camera offline' | systemd-cat -p info
+fi
